914eb57d306e76a05eb29c679fd9f9a148aec032
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-09-07 23:40:50 +0100

    code redo

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from .views import list_books , LibraryDetailView
 from . import views
 
+
 urlpatterns = [
     path('books/', list_books, name="list_books"),
     path('library/<int:pk>/', LibraryDetailView.as_view, name="library_detail"),
     path('login/', LoginView.as_view(template_name="login")),
     path('register/', views.register_view(template_name="register")),
     path('logout/', LogoutView.as_view(template_name="logout")),
-
+    path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view"),
 ]
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.views.generic.detail import DetailView
 from .models import Library, Book
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 
 # Function-based view to list all books
@@ -56,3 +57,34 @@ def logout_view(request):
     logout (request)
     messages.info((request, "You have successfully logged out!"))
     return render(request, "relationship_app/logout.html")
+
+
+
+# Helper functions to check user roles
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+
+# Views restricted by roles
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
