ae4c60e30fc0ec4239734d7260351080b936fa14
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-09-07 19:17:31 +0100

    listview and librarydetail templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import *
 
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.all()
+    output = ""
+  
+
+    for book in books:
+        output += f"{book.title} by {book.author.name}"
+    
+
+    return render(output)
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context ["books"] = self.objects.book.all() #fetch books for this library
+        return context
