ac282a0c266d5224e23dbf0e6f8819a6b8ffe95e
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-09-07 16:55:31 +0100

    Code addition

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,4 +8,4 @@ Library.objects.get(name=library_name)
 
 #Retrieve the librarian for a library.
 librarian = library.librarian
-library.objects.get(name=library_name)
+librarian.objects.get(library=)
