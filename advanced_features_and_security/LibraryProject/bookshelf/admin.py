adbe1ced0ec62275898f15ce944cc65d31b05302
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-09-15 23:58:24 +0100

    Refactor user model to use CustomUser from bookshelf app and implement CustomUserManager

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+
+
+# Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
 
-# Register your models here.
-admin.site.register(Book)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    ordering = ('email',)   # instead of ('username',)
+
+
+#extends the default UserAdmin fieldsets
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields":("date_of_birth", "profile_photo")}),
+    )
+
+admin.site.register(Book, CustomUser, CustomUserAdmin)
+
+
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import  AbstractUser, BaseUserManager
 
 # Create your models here.
 class Book(models.Model):
@@ -7,4 +8,39 @@ class Book(models.Model):
     publication_year = models.IntegerField()
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user (self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError ("The email field is required.")
+        email = self.normalize_email(email)
+        user = self.model (email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser (self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError ("User should have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError ("User should have is_superuser=True.")
+        
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = None #..................Username is set to None because we want to use email as the login criteria
+
+    #Enforce unique email
+    email = models.EmailField(unique=True)
+
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'  # now email is used for login
+    REQUIRED_FIELDS = []   # you can add others if you want them required (e.g. ["date_of_birth"])
 
