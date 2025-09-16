b642345ea12541d0527364f85374d438874e2509
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-09-16 18:56:23 +0100

    Refactor forms to replace SearchForm with ExampleForm for Book model

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from.models import Book 
 
-class SearchForm(forms.Form):
-    query = forms.CharField(max_length=100)
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'description']  
 
