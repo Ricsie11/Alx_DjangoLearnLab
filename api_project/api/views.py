1ade76acd23594472533af9b42921f58dc71998d
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-09-23 13:27:33 +0100

    add authentication and permission setup using DRF TokenAuth

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -11,4 +13,12 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated] #Only logged in user can access
 
+    def get_queryset(self):
+        #Only show books for the logged in user
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        #Automatically assign the logged-in user to the book
+        serializer.save(user=self.request.user)
