90ed956cf594a75933706604934805275252e039
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-09-28 19:07:52 +0100

    implement BookSerializer with nested AuthorSerializer and publication year validation

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import *
+from datetime import date
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'  ##....To list all fields in the Author model 
+
+
+class BookSerializer(serializers.ModelSerializer): #...Used ModelSerializer for a fine grained CRUD experince
+    author = AuthorSerializer() #...Nested AuthorSerializer to display author name within the book serializer
+    class Meta:
+        model = Book
+        fields = '__all__' #....To list all fields in the Book model 
+
+    def validate_publication_year(self, value):
+        if value > date.today().year:
+            raise serializers.ValidationError ("Publication year cannot be in the future.")
+        return value
