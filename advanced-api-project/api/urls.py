cf5edec9218c5cd9af47dacc5626019aa3caccfa
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-09-29 23:28:48 +0100

    Implement API endpoint protection using Django REST Framework's permission classes, adding IsAuthenticated and IsAuthenticatedOrReadOnly permissions to restrict access based on user roles and authentication status

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,17 +3,17 @@ from .views import *
 
 urlpatterns = [
     # Author URLs
-    path('author/', AuthorListView.as_view(), name="author_list"),
-    path('author/create/', AuthorCreateView.as_view(), name="author_create"),
-    path('author/<int:pk>/', AuthorDetailView.as_view(), name="author_detail"),
-    path('author/<int:pk>/update/', AuthorUpdateView.as_view(), name="author_update"),
-    path('author/<int:pk>/delete/', AuthorDeleteView.as_view(), name="author_delete"),
+    path('authors/', AuthorListView.as_view(), name="author_list"),
+    path('authors/create/', AuthorCreateView.as_view(), name="author_create"),
+    path('authors/<int:pk>/', AuthorDetailView.as_view(), name="author_detail"),
+    path('authors/<int:pk>/update/', AuthorUpdateView.as_view(), name="author_update"),
+    path('authors/<int:pk>/delete/', AuthorDeleteView.as_view(), name="author_delete"),
 
 
     # Book URLs
-    path('book/', BookListView.as_view(), name="book_list"),
-    path('book/create/', BookCreateView.as_view(), name="book_create"),
-    path('book/<int:pk>/', BookDetailView.as_view(), name="book_detail"),
-    path('book/<int:pk>/update/', BookUpdateView.as_view(), name="book_update"),
-    path('book/<int:pk>/delete/', BookDeleteView.as_view(), name="book_delete"),
+    path('books/', BookListView.as_view(), name="book_list"),
+    path('books/create/', BookCreateView.as_view(), name="book_create"),
+    path('books/<int:pk>/', BookDetailView.as_view(), name="book_detail"),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name="book_update"),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name="book_delete"),
 ]
