2fed3eee9d43ea53f2927e2e9bf932bd6f5646da
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-10-06 21:15:43 +0100

    Implemented blog post list view with search functionality, added CSS styling, and included JavaScript code for search form enhancement

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post, CustomUser
+from .models import Post, CustomUser, Comment, PostIndex
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(CustomUser)
+admin.site.register(Comment)
+admin.site.register(PostIndex)
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User, AbstractUser
 from django.conf import settings
 from taggit.managers import TaggableManager
 from haystack import indexes
