98b02b3775a99833624f25011e9f470821a88c5a
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-10-08 13:15:46 +0100

    Add token creation and dynamic user handling to RegisterSerializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.authtoken.models import Token
 from .models import CustomUser
 
 
@@ -11,12 +12,17 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        # Used get_user_model() to dynamically get the active User model
+        user = get_user_model().objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
             password=validated_data["password"],
-            bio=validated_data.get("bio")
+            bio=validated_data.get("bio") 
         )
+
+        # Create a token for the new user
+        Token.objects.create(user=user)
+
         return user
 
 
@@ -29,4 +35,4 @@ class LoginSerializer(serializers.Serializer):
         if user and user.is_active:
             return user
         raise serializers.ValidationError("Invalid credentials")
-         
+
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,21 +3,20 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer, LoginSerializer
-from .models import CustomUser
 
-# Create your views here.
+
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
             token, created = Token.objects.get_or_create(user=user)
-            return Response ({
+            return Response({
                 "user": serializer.data,
                 "token": token.key
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
 
 class LoginView(APIView):
     def post(self, request):
@@ -25,7 +24,7 @@ class LoginView(APIView):
         if serializer.is_valid():
             user = serializer.validated_data
             token, created = Token.objects.get_or_create(user=user)
-            return Response ({
+            return Response({
                 "token": token.key
-            })
-        return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
