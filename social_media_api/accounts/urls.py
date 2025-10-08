02858e2bd0e556b4dee9d8b289153b9c511fdc4a
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-10-08 13:16:59 +0100

    Add token creation and dynamic user handling to RegisterSerializer

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RegisterView, LoginView
 
 urlpatterns = [
-    path('Register/', RegisterView.as_view(), name="register"),
-    path('Login/', LoginView.as_view(), name="Login"),
+    path('register/', RegisterView.as_view(), name="register"),
+    path('login/', LoginView.as_view(), name="Login"),
 ]
